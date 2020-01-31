ce2c65fe684a9b34024a4c63bbdba73656406c51
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-01-31 10:34:21 -0600

    button click

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,21 @@
 $( document ).ready( onReady );
 
+// songs app
+let songs = [];
+
 function onReady(){
     console.log( 'in onReady' );
+    $( '#updateSongsButton' ).on( 'click', displaySongs );
 } // end onReady
+
+function displaySongs(){
+    console.log( 'in displaySongs' );
+} // end displaySongs
+
+/// - test song
+songs.push( { 
+    artist: 'Red Hot Chili Peppers',
+    song: 'Under the Bridge',
+    album: 'Blood Sugar Sex Magik',
+    year: 1991
+});
