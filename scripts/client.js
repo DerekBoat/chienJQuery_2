e1df72e626cdb421316bcae0f8a32b0b36d023f2
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-01-31 10:46:43 -0600

    loop append

--- scripts/client.js
+++ scripts/client.js
@@ -10,12 +10,21 @@ function onReady(){
 
 function displaySongs(){
     console.log( 'in displaySongs' );
+    // target an output element by ID
+    let el = $( '#songsOut');
+    // empty out element
+    el.empty();
+    // loop through songs array
+    for( let i=0; i<songs.length; i++ ){
+        // append each song to DOM
+        el.append( `<li>${ songs[i].title }: ${ songs[i].artist }, ${ songs[i].album } (${ songs[i].year })</li>`);
+    } // end for
 } // end displaySongs
 
 /// - test song
 songs.push( { 
     artist: 'Red Hot Chili Peppers',
-    song: 'Under the Bridge',
+    title: 'Under the Bridge',
     album: 'Blood Sugar Sex Magik',
     year: 1991
 });
